734386b17cd373d1ba8576d1573f5a022a0cb7cb
KingTingTheGreat <jting@bu.edu>
2023-04-10 14:31:39 -0400

    added a command to use the bot to send messages

--- help_cog.py
+++ help_cog.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from ast import alias
 
 class help_cog(commands.Cog):
     def __init__(self, bot):
@@ -28,7 +29,6 @@ General commands:
             for channel in guild.text_channels:
                 if channel.name == "general":
                     self.text_channel_list.append(channel)
-
         await self.send_to_all(self.help_message)        
 
     @commands.command(name="help", help="Displays all the available commands")
@@ -39,3 +39,34 @@ General commands:
     async def send_to_all(self, msg):
         for text_channel in self.text_channel_list:
             await text_channel.send(msg)
+
+    @commands.command(name="send_message", aliases=["sm"], help="Sends a message to all the text channels")
+    async def send_message(self, ctx, *args):
+        """
+        format of command call: .sm server%$channel%$message
+        """
+        # check if user is admin (or ridoot)
+        if ctx.author.guild_permissions.administrator == False and ctx.author.name != 'ridoot':
+            return
+        await ctx.send(f"Hello, {ctx.author.name}")
+        full_arg = ' '.join(args)
+        if full_arg.count('%$') != 2:
+            await ctx.send("Invalid args. Please try again.")
+            return
+        target_server, target_channel, message = full_arg.split('%$')
+        # find server/guild
+        for guild in self.bot.guilds:
+            if guild.name != target_server:
+                continue
+            if ctx.author not in guild.members:
+                # user is not in this server
+                await ctx.send("You must be in the specified server")
+                return
+            # find channel
+            for channel in guild.text_channels:
+                if channel.name != target_channel:
+                    continue
+                # send message
+                await channel.send(message)
+            
+        
